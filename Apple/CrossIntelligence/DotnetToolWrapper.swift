271788c48e1bf08e8c67f323f6930b0adeee3aaa
Frank A. Krueger <fak@praeclarum.org>
2025-08-01 17:13:42 -0700

    Codegen tool wrappers

--- Apple/CrossIntelligence/DotnetToolWrapper.swift
+++ Apple/CrossIntelligence/DotnetToolWrapper.swift
@@ -14,82 +14,26 @@ struct DefaultDotnetArgs {
     
 }
 
-fileprivate let maxTools = 100
-
-@available(iOS 26.0, macOS 26.0, macCatalyst 26.0, visionOS 26.0, *)
-var argsSchemas: [GenerationSchema] = Array(repeating: DefaultDotnetArgs.generationSchema, count: maxTools)
-
 protocol DotnetToolWrapper {
-    var isFree: Bool { get set }
     var tool: DotnetTool? { get set }
+    var index: Int { get }
 }
-
-@available(iOS 26.0, macOS 26.0, macCatalyst 26.0, visionOS 26.0, *)
-struct DotnetToolWrapper0: Tool, DotnetToolWrapper {
-    var tool: DotnetTool?
-    var isFree: Bool = true
-    var name: String { tool?.name ?? "" }
-    var description: String { tool?.description ?? "" }
-    struct Arguments: Generable {
-        let content: GeneratedContent
-        static var generationSchema: GenerationSchema { argsSchemas[0] }
-        var generatedContent: GeneratedContent { content }
-        init(_ content: GeneratedContent) throws {
-            self.content = content
-        }
-    }
-    func call(arguments: Arguments) async throws -> some PromptRepresentable {
-        let argsJson = "{}"
-        return tool?.call(arguments: argsJson)
-    }
-}
-
-@available(iOS 26.0, macOS 26.0, macCatalyst 26.0, visionOS 26.0, *)
-struct DotnetToolWrapper1: Tool, DotnetToolWrapper {
-    var tool: DotnetTool?
-    var isFree: Bool = true
-    var name: String { tool?.name ?? "" }
-    var description: String { tool?.description ?? "" }
-    struct Arguments: Generable {
-        let content: GeneratedContent
-        static var generationSchema: GenerationSchema {
-            argsSchemas[1]
-        }
-        init(_ content: GeneratedContent) throws {
-            self.content = content
-        }
-        var generatedContent: GeneratedContent {
-            content
-        }
-    }
-    func call(arguments: Arguments) async throws -> some PromptRepresentable {
-        let argsJson = "{}"
-        return tool?.call(arguments: argsJson)
-    }
-}
-
-@available(iOS 26.0, macOS 26.0, macCatalyst 26.0, visionOS 26.0, *)
-var tools: [any Tool & DotnetToolWrapper] = [
-    DotnetToolWrapper0(tool: nil),
-    DotnetToolWrapper1(tool: nil),
-]
 
 @available(iOS 26.0, macOS 26.0, macCatalyst 26.0, visionOS 26.0, *)
 fileprivate func allocTool(dotnetTool: DotnetTool) -> any Tool & DotnetToolWrapper {
-    for i in 0..<tools.count {
-        if tools[i].isFree {
-            argsSchemas[i] = DefaultDotnetArgs.generationSchema
-            tools[i].isFree = false
-            tools[i].tool = dotnetTool
-            return tools[i]
+    for i in 0..<gTools.count {
+        if gTools[i].tool == nil {
+            gArgsSchemas[i] = DefaultDotnetArgs.generationSchema
+            gTools[i].tool = dotnetTool
+            return gTools[i]
         }
     }
-    return tools[tools.count - 1] // Return the last tool if no free tool is found
+    return gTools[gTools.count - 1] // Return the last tool if no free tool is found
 }
 
 @available(iOS 26.0, macOS 26.0, macCatalyst 26.0, visionOS 26.0, *)
 fileprivate func freeTool(index: Int) {
-    tools[index].isFree = true
+    gTools[index].tool = nil
 }
 
 @objc(DotnetTool)
