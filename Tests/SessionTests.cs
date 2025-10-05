1842b8f40525784e7d69b775f277fb29b051595e
Frank A. Krueger <fak@praeclarum.org>
2025-10-05 10:34:06 -0700

    Add Tests project for #11

---
+++ Tests/SessionTests.cs
@@ -0,0 +1,20 @@
+﻿using CrossIntelligence;
+
+namespace Tests;
+
+public class SessionTests
+{
+#if __MACOS__ || __IOS__ || __MACCATALYST__ || __TVOS__
+    [Fact]
+    public void AppleIntelligenceAvailable()
+    {
+        Assert.True(IntelligenceSession.IsAppleIntelligenceAvailable);
+    }
+#else
+    [Fact]
+    public void AppleIntelligenceNotAvailable()
+    {
+        Assert.False(IntelligenceSession.IsAppleIntelligenceAvailable);
+    }
+#endif
+}
