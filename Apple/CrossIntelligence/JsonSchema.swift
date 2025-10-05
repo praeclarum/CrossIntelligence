cd98721006840547e572706ae072aef63fd15fd6
Frank A. Krueger <fak@praeclarum.org>
2025-10-05 09:26:33 -0700

    Support array, numbers, and property-less objects

--- Apple/CrossIntelligence/JsonSchema.swift
+++ Apple/CrossIntelligence/JsonSchema.swift
@@ -12,34 +12,35 @@ fileprivate class JsonSchema: Codable {
     var type: String
     var title: String?
     var properties: [String: JsonSchema]?
+    var items: JsonSchema?
     var required: [String]?
+    
+    init(type: String) {
+        self.type = type
+    }
 }
 
 @available(iOS 26.0, macOS 26.0, macCatalyst 26.0, visionOS 26.0, *)
-fileprivate func readDynamicJsonSchema(_ schema: JsonSchema) -> DynamicGenerationSchema? {
-    if let properties = schema.properties {
+fileprivate func readDynamicJsonSchema(_ schema: JsonSchema) -> DynamicGenerationSchema {
+    switch schema.type {
+    case "array":
+        let items = schema.items ?? JsonSchema(type: "string")
+        return DynamicGenerationSchema.init(arrayOf: readDynamicJsonSchema(items))
+    case "integer":
+        return DynamicGenerationSchema(type: Int.self)
+    case "number":
+        return DynamicGenerationSchema(type: Double.self)
+    case "object":
+        let properties = schema.properties ?? [:]
         let props = properties.compactMap { (key, value) -> DynamicGenerationSchema.Property? in
-            if let nestedSchema = readDynamicJsonSchema(value) {
-                return DynamicGenerationSchema.Property(name: key, schema: nestedSchema)
-            }
-            else {
-                print("Unsupported JSON schema property: \(key) with type \(value.type)")
-                return nil
-            }
+            return DynamicGenerationSchema.Property(name: key, schema: readDynamicJsonSchema(value))
         }
         return DynamicGenerationSchema(name: schema.title ?? "Unnamed", properties: props)
-    }
-    else {
-        // Primitive type
-        switch schema.type {
-        case "string":
-            return DynamicGenerationSchema(type: String.self)
-        case "integer":
-            return DynamicGenerationSchema(type: Int.self)
-        default:
-            print("Unsupported JSON schema type: \(schema.type)")
-            return nil
-        }
+    case "string":
+        return DynamicGenerationSchema(type: String.self)
+    default:
+        print("Unsupported JSON schema type: \"\(schema.type)\"")
+        return DynamicGenerationSchema(type: String.self)
     }
 }
 
@@ -57,9 +58,6 @@ fileprivate func parseDynamicJsonSchema(_ json: String) -> DynamicGenerationSche
 @available(iOS 26.0, macOS 26.0, macCatalyst 26.0, visionOS 26.0, *)
 func parseJsonSchema(_ json: String) -> GenerationSchema? {
     let dschema = parseDynamicJsonSchema(json)
-    guard let dschema else {
-        return nil
-    }
     guard let schema = try? GenerationSchema(root: dschema, dependencies: []) else {
         print("Failed to create GenerationSchema from DynamicGenerationSchema: \(json)")
         return nil
